f7bae4475d3fa97dc1b0b06c7de07d1bab4429a0
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2025-10-14 14:39:54 +0200

    update: changed server solution position

---
+++ ImmerseYourselfServer/ImmerseYourselfServer/ImmerseYourselfServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ImmerseYourselfServer
+{
+    class ServerProgram
+    {
+        [DllImport("user32.dll")]
+        static extern bool EnumDisplayDevices(string lpDevice, uint iDevNum, ref DISPLAY_DEVICE lpDisplayDevice, uint dwFlags);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DISPLAY_DEVICE
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public int cb;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string DeviceName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string DeviceString;
+            [MarshalAs(UnmanagedType.U4)]
+            public int StateFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string DeviceID;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string DeviceKey;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "ImmerseYourselfServer";
+            
+            int count = 0;
+            DISPLAY_DEVICE device = new DISPLAY_DEVICE();
+            device.cb = Marshal.SizeOf(device);
+
+            while (EnumDisplayDevices(null, count, ref device, 0))
+            {
+                count++;
+                device.cb = Marshal.SizeOf(device);
+            }
+            
+            Console.WriteLine($"Number of monitors: {count}");
+            
+            Server.Start(5);
+            
+            Console.ReadKey();
+        }
+    }
+}
